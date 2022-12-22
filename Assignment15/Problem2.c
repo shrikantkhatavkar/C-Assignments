6e17e0d55e33dbea215f4b58b954e71a61a478fa
unknown <shrikant.shri.srk@gmail.com>
2022-12-22 08:14:27 +0530

    adding files

---
+++ Assignment15/Problem2.c
@@ -0,0 +1,44 @@
+//////////////////////////////////////////
+////////////////// Name : Shrikant
+///////////////// Date : 22-12-22
+//////////////////////////////////////////
+
+//2. Accept number of rows and number of columns from user and display below pattern.
+//Input : iRow = 4 iCol = 4
+//Output : 
+//* * * #
+//* * # @
+//* # @ @
+//# @ @ @
+
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+	for(int i = 1; i<=iRow;i++)
+	{
+		for(int j = iCol;j>=1;j--)
+		{
+			if(i==j)
+			{
+				printf("# ");
+			}
+			if(i<j)
+			{
+				printf("* ");
+			}
+			if(i>j)
+			{
+				printf("@ ");
+			}
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+int iValue1 = 0, iValue2 = 0;
+printf("Enter number of rows and columns\n");
+scanf("%d %d",&iValue1, &iValue2);
+Pattern(iValue1, iValue2);
+return 0;
+}
---
+++ Assignment15/Problem4.c
@@ -0,0 +1,46 @@
+//////////////////////////////////////////
+////////////////// Name : Shrikant
+///////////////// Date : 22-12-22
+//////////////////////////////////////////
+
+//4. Accept number of rows and number of columns from user and display below pattern.
+//Input : iRow = 6 iCol = 6
+//Output : 
+//* * * * * *
+//* # # # * *
+//* # # * $ *
+//* # * $ $ *
+//* * $ $ $ *
+//* * * * * *
+
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+	for(int i = 1; i<= iRow;i++)
+	{
+		for(int j = iCol; j>= 1;j--)
+		{
+			if( (i ==1) || (i==iRow) || (j == 1) || (j ==iCol) || (i== j) )
+			{
+				printf("* ");
+			}
+			else if(i<j)
+			{
+				printf("# ");
+			}
+			else if(i>j)
+			{
+				printf("$ ");
+			}
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+int iValue1 = 0, iValue2 = 0;
+printf("Enter number of rows and columns\n");
+scanf("%d %d",&iValue1, &iValue2);
+Pattern(iValue1, iValue2);
+return 0;
+}
